9eda23c8bfe3c60613884587e9cc333166957e9b
Mikhail Kolotov <warmenzim@mail.ru>
2019-03-02 19:25:41 +0300

    Добавлен пример работы с файлами

---
+++ 5.Использование DirectoryInfo/DirectoryInfo/DirectoryInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+
+namespace DirectoryInfos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //работа с каталогами 
+            //класс DirectoryInfo
+            DirectoryInfo direct = new DirectoryInfo(@"C:\DirectoryInfos");//@ указывает несуществующий каталог
+            direct.Create();//создаем каталог
+            direct.CreateSubdirectory("Data");//создать подкатолог
+            direct.CreateSubdirectory("Help/Me/Please");
+
+            //рассмотрим свойства каталога с помощью методов унаследованных от FileSystemInfo
+            Console.WriteLine("FullName: {0}", direct.FullName);
+            Console.WriteLine("Name: {0}", direct.Name);
+            Console.WriteLine("Parent: {0}", direct.Parent);
+            Console.WriteLine("CreationTime: {0}", direct.CreationTime);
+            Console.WriteLine("Attributes: {0}", direct.Attributes);
+            Console.WriteLine("Root: {0}", direct.Root);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 6.Удаление файлов по расширению/DeleteIt/DeleteIt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DeleteIt
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Привет! Напиши путь к папке в которой искать и нажми Enter");
+            string path = Console.ReadLine();            
+            DirectoryInfo directory = new DirectoryInfo(path);
+           
+            if(path != null )
+            {
+                Console.WriteLine("Понял. Искать в каталоге {0}",path);
+                Console.WriteLine("Напиши какой формат файлов нужно найти и удалить?");
+                string format = "*."+ Console.ReadLine();             
+
+                if (format != null)
+                {
+                    FileInfo[] files = directory.GetFiles(format, SearchOption.AllDirectories);
+                    foreach (FileInfo f in files)
+                    {
+                        Console.WriteLine("Найдено файлов {0}", files.Length);
+                        Console.WriteLine("Имя каталога: {0}", directory.Name);
+                        Console.WriteLine("Аттрибут файла: {0}", directory.Attributes);
+                        Console.WriteLine("Создан: {0}", directory.CreationTime);
+                        f.Delete();
+                    }
+                    Console.WriteLine("Эти файл/ы были удалены");
+                    Console.ReadLine();                   
+                }
+                else
+                {
+                    Console.WriteLine("Не указан формат");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ты не указал путь :( ");
+            }
+        }
+    }
+}
