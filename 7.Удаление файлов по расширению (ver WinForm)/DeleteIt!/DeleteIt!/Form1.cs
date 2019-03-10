0578da655c997bbfcb916a7938ad794f3ce0f670
Mikhail Kolotov <warmenzim@mail.ru>
2019-03-10 20:53:59 +0300

    Add commit

--- 7.Удаление файлов по расширению (ver WinForm)/DeleteIt!/DeleteIt!/Form1.cs
+++ 7.Удаление файлов по расширению (ver WinForm)/DeleteIt!/DeleteIt!/Form1.cs
@@ -7,25 +7,25 @@ namespace DeleteIt_
 {
     public partial class Form1 : Form
     {        
-        DirectoryInfo Direction { get; set; }        
-        FileInfo[] Files { get; set; }
+        DirectoryInfo Direction { get; set; }//автосвойство типа DirectoryInfo с именем Direction        
+        FileInfo[] Files { get; set; }//автосвойство типа FileInfo с именем Files
 
         public Form1()
         {
             InitializeComponent();
-            textBox1.ReadOnly = true;
+            textBox1.ReadOnly = true;//установить параметр textBox1 в режим Только чтение, чтобы пользователь не смог изменить путь с клавиатуры
         }
 
         public void button1_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog FBD = new FolderBrowserDialog();
-            FBD.ShowNewFolderButton = false;
+            FolderBrowserDialog FBD = new FolderBrowserDialog();//тип FolderBrowserDialog с именем FBD позволяет открыть диалоговое окно
+            FBD.ShowNewFolderButton = false;//запрет на кнопку создания новой папки
             
-            if (FBD.ShowDialog() == DialogResult.OK)
+            if (FBD.ShowDialog() == DialogResult.OK)//если нажали ОК то...
             {
-                string path = FBD.SelectedPath;
+                string path = FBD.SelectedPath;//присвоить значение для строки path тому, чему равен выбранный путь
                 Direction = new DirectoryInfo(path);//заполняем каталог Direction выбранным путём path
-                textBox1.Text = path;              
+                textBox1.Text = path;//textBox1 заполняем path чтобы путь был виден пользователю              
             }
         }      
 
@@ -35,7 +35,7 @@ namespace DeleteIt_
 
         private void button2_Click(object sender, EventArgs e)
         {
-            try{
+            try{//оборачиваю в try catch на случай ошибочного пути
                 if (Direction.Exists) {//проверяем каталог на коректность, если true то...
 
                     //используем FileInfo для нахождения всех файлов в каталоге и подкаталогах
@@ -44,9 +44,9 @@ namespace DeleteIt_
                     foreach (FileInfo f in Files)//из найденых строим список с чекбоксами в checkedListBox1
                     {
                         label4.Text = checkedListBox1.Items.Count.ToString();//отсчет с нуля 
-                        //checkedListBox1.Items.Add(f.ToString());
-                        checkedListBox1.Items.Add(f.FullName);
-                        checkedListBox1.SetSelected(0, true);
+                        
+                        checkedListBox1.Items.Add(f.FullName);//получаем полный путь до файлов и отображаем его в списке
+                        checkedListBox1.SetSelected(0, true);//устанавливаем 0 для того, чтобы не было отмечено ни одного файла в списке
                     }
                     if (checkedListBox1.Items.Count == 0)
                     {//кнопка скана активна только если список пуст
@@ -56,22 +56,22 @@ namespace DeleteIt_
                 }
             }
             catch { 
-                MessageBox.Show("Укажите путь");
+                MessageBox.Show("Укажите путь");//в случае если путь не указан или же является некорректным вывести окно с ошибкой
             }
         }
         
         private void button4_Click(object sender, EventArgs e)
         {
-            checkedListBox1.Items.Clear();
-            textBox1.Clear();
-            Direction = null;
-            button2.Enabled = true;
+            checkedListBox1.Items.Clear();//очистить список
+            textBox1.Clear();//очистить строку с отобразившимся путём
+            Direction = null;//очистить переменную которая хранит каталог 
+            button2.Enabled = true;//активировать кнопку Сканировать
         }
 
         private void button3_Click(object sender, EventArgs e)
         {          //отметить всё  
-                for (int i = 0; i < checkedListBox1.Items.Count; i++)
-                checkedListBox1.SetItemChecked(i, true);                
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)//пройтись циклом по всем элементам 
+                checkedListBox1.SetItemChecked(i, true);//и установить для каждого значение "галочку"                
             
             /*  //снять все галочки
                  for (int i = 0; i < checkedListBox1.Items.Count; i++)
@@ -82,14 +82,13 @@ namespace DeleteIt_
 
         private void button5_Click(object sender, EventArgs e)
         {
-            for (int i = checkedListBox1.Items.Count - 1; i >= 0; i--)
+            for (int i = checkedListBox1.Items.Count - 1; i >= 0; i--)//пройтись по всем элементам циклом
             {
-                if (checkedListBox1.GetItemChecked(i))
-                {
-                    
-                    var pathFile = checkedListBox1.Items[i].ToString();
-                    File.Delete(pathFile);
-                    checkedListBox1.Items.RemoveAt(i);
+                if (checkedListBox1.GetItemChecked(i))//и если элемент отмечен галочкой
+                {                    
+                    var pathFile = checkedListBox1.Items[i].ToString();//заполнить переменную пути к файлу
+                    File.Delete(pathFile);//удалить файл лежащий по пути pathFile
+                    checkedListBox1.Items.RemoveAt(i);//удалить элемент из списка
                 }
             }
         }        
