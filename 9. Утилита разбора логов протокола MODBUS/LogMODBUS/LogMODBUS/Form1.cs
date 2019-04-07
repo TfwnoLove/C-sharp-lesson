9f40a073620551d199139aa32e9cceeadf7fbfba
Mikhail Kolotov <warmenzim@mail.ru>
2019-04-07 14:40:09 +0300

    Распарсил лог, заполнил им таблицу

--- 9. Утилита разбора логов протокола MODBUS/LogMODBUS/LogMODBUS/Form1.cs
+++ 9. Утилита разбора логов протокола MODBUS/LogMODBUS/LogMODBUS/Form1.cs
@@ -75,57 +75,13 @@ namespace LogMODBUS
         }
 
         public void parseText()
-        {           
+        {
             List<LogProperties> Tables = new List<LogProperties>();
             //взять n-ную строку           
-            string logText = GetString(0, Direction.FullName);//Direction.FullName автосвойство хранящие в себе путь к файлу
-            //распарсить строку
-            string[] parsing = logText.Split(new char[] { '\t'});
+            
+            IEnumerable<string> result = File.ReadLines(Direction.FullName);
 
             LogProperties logProp = new LogProperties();
-            //эти свойства хранят данные из лога 1й строки
-            logProp.Number = parsing[0];
-            logProp.Time = parsing[1];
-            logProp.AppName = parsing[2];
-            logProp.IRP = parsing[3];//IRP
-            logProp.Serial0 = parsing[4];//serial
-            logProp.ErrorType = parsing[5];//error type
-
-            Regex regexLength = new Regex(@"Length (\w*)");//for logProp.Length
-            var mLength = regexLength.Matches(logText);
-            foreach (Match match in mLength)
-            {
-                var m = match.Groups[1].Value;
-                logProp.Length = m;
-            }
-
-            Regex regexAdr = new Regex(@": (\w*)");//for logProp.Adress
-            var mAdr = regexAdr.Matches(logText);
-            foreach (Match match in mAdr)
-            {
-                var m = match.Groups[1].Value;
-                logProp.Adress = m;
-            }
-
-            Regex regexCommand = new Regex(logProp.Adress +@" (\w*)");//for logProp.Command
-            var mCom = regexCommand.Matches(logText);
-            foreach (Match match in mCom)
-            {
-                var m = match.Groups[1].Value;
-                logProp.Command = m;
-            }
-
-            string logTextCopy = GetString(0, Direction.FullName).Replace(" ", string.Empty);//
-            logProp.Crc = logTextCopy.Substring(logTextCopy.Length - 5);
-
-
-            logProp.Raw_frame = logTextCopy.Substring(logTextCopy.Length-(Convert.ToInt32(logProp.Length)*2+1));
-
-            
-            string forRdata = logProp.Raw_frame.Remove(0, 4);
-            logProp.Raw_data = forRdata.Remove(forRdata.Length - 5);
-            
-
             //заполняю хедер
             dataGridView1.Columns.Add(logProp.Number, "Number");
             dataGridView1.Columns.Add(logProp.Time, "Time");
@@ -133,42 +89,91 @@ namespace LogMODBUS
             dataGridView1.Columns.Add(logProp.IRP, "IRP");
             dataGridView1.Columns.Add(logProp.Serial0, "Serial0");
             dataGridView1.Columns.Add(logProp.ErrorType, "ErrorType");
-            dataGridView1.Columns.Add(logProp.Length.ToString(), "Length");
+            dataGridView1.Columns.Add(logProp.Length, "Length");
             dataGridView1.Columns.Add(logProp.Adress, "Adress");
             dataGridView1.Columns.Add(logProp.Command, "Command");
             dataGridView1.Columns.Add(logProp.Crc, "Crc");
             dataGridView1.Columns.Add(logProp.Raw_frame, "Raw_frame");
             dataGridView1.Columns.Add(logProp.Raw_data, "Raw_data");
 
-
-            //заполняю ячейки данными полученными из свойств
-            int rowNumber = dataGridView1.Rows.Add();
-            dataGridView1.Rows[rowNumber].Cells[logProp.Number].Value = logProp.Number;
-            dataGridView1.Rows[rowNumber].Cells[logProp.Time].Value = logProp.Time;
-            dataGridView1.Rows[rowNumber].Cells[logProp.AppName].Value = logProp.AppName;
-            dataGridView1.Rows[rowNumber].Cells[logProp.IRP].Value = logProp.IRP;
-            dataGridView1.Rows[rowNumber].Cells[logProp.Serial0].Value = logProp.Serial0;
-            dataGridView1.Rows[rowNumber].Cells[logProp.ErrorType].Value = logProp.ErrorType;
-            dataGridView1.Rows[rowNumber].Cells[logProp.Length].Value = logProp.Length;
-            dataGridView1.Rows[rowNumber].Cells[logProp.Adress].Value = logProp.Adress;
-            dataGridView1.Rows[rowNumber].Cells[logProp.Command].Value = logProp.Command;
-            dataGridView1.Rows[rowNumber].Cells[logProp.Crc].Value = logProp.Crc;
-            dataGridView1.Rows[rowNumber].Cells[logProp.Raw_frame].Value = logProp.Raw_frame;
-            dataGridView1.Rows[rowNumber].Cells[logProp.Raw_data].Value = logProp.Raw_data;
-        }
-        public string GetString(int numberString,string filePath)
-        {
-            IEnumerable<string> result = File.ReadLines(filePath).Skip(numberString).Take(1);
-
-            string newString = null;
-
-            foreach (string str in result)
+            foreach (string logText in result)
             {
-                newString += str;
-            }
+                //распарсить строку
+                string[] parsing = logText.Split(new char[] { '\n','\t' }, StringSplitOptions.RemoveEmptyEntries);
 
-            return newString;
-        }
+                //эти свойства хранят данные из лога 1й строки
+                logProp.Number = parsing[0];
+                logProp.Time = parsing[1];
+                logProp.AppName = parsing[2];
+                logProp.IRP = parsing[3];//IRP
+                logProp.Serial0 = parsing[4];//serial
+                logProp.ErrorType = parsing[5];//error type
+
+                Regex regexLength = new Regex(@"Length (\w*)");//for logProp.Length
+                var mLength = regexLength.Matches(logText);
+                foreach (Match match in mLength)
+                {
+                    var m = match.Groups[1].Value;
+                    logProp.Length = m;
+                }
+
+                Regex regexAdr = new Regex(@": (\w*)");//for logProp.Adress
+                var mAdr = regexAdr.Matches(logText);
+                foreach (Match match in mAdr)
+                {
+                    var m = match.Groups[1].Value;
+                    logProp.Adress = m;
+                }
+
+                Regex regexCommand = new Regex(logProp.Adress + @" (\w*)");//for logProp.Command
+                var mCom = regexCommand.Matches(logText);
+                foreach (Match match in mCom)
+                {
+                    var m = match.Groups[1].Value;
+                    logProp.Command = m;
+                }
+
+                string logTextCopy = logText.Replace(" ", string.Empty);//
+
+                logProp.Raw_frame = logTextCopy.Substring(logTextCopy.Length - (Convert.ToInt32(logProp.Length) * 2 + 1));
+
+                if (logProp.Length == "0")
+                {
+                    logProp.Crc = "";
+                }
+                else if (logProp.Length != "0" && logProp.Raw_frame.Length>3)
+                {
+                    logProp.Crc = logTextCopy.Substring(logTextCopy.Length - 5);
+                }
+                else logProp.Crc = logTextCopy.Substring(logTextCopy.Length - 3);
+
+                try
+                {
+                    if (logProp.Command != "")
+                    {
+                        string forRdata = logProp.Raw_frame.Remove(0, 4);
+                        logProp.Raw_data = forRdata.Remove(forRdata.Length - 4);
+                    }
+                }
+                catch {
+                    logProp.Raw_data = "Error!";
+                }
+                //заполняю ячейки данными полученными из свойств
+                int rowNumber = dataGridView1.Rows.Add();
+                dataGridView1.Rows[rowNumber].Cells[0].Value = logProp.Number;
+                dataGridView1.Rows[rowNumber].Cells[1].Value = logProp.Time;
+                dataGridView1.Rows[rowNumber].Cells[2].Value = logProp.AppName;
+                dataGridView1.Rows[rowNumber].Cells[3].Value = logProp.IRP;
+                dataGridView1.Rows[rowNumber].Cells[4].Value = logProp.Serial0;
+                dataGridView1.Rows[rowNumber].Cells[5].Value = logProp.ErrorType;
+                dataGridView1.Rows[rowNumber].Cells[6].Value = logProp.Length;
+                dataGridView1.Rows[rowNumber].Cells[7].Value = logProp.Adress;
+                dataGridView1.Rows[rowNumber].Cells[8].Value = logProp.Command;
+                dataGridView1.Rows[rowNumber].Cells[9].Value = logProp.Crc;
+                dataGridView1.Rows[rowNumber].Cells[10].Value = logProp.Raw_frame;
+                dataGridView1.Rows[rowNumber].Cells[11].Value = logProp.Raw_data;
+            }
+        }        
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
@@ -177,26 +182,24 @@ namespace LogMODBUS
 
         public void button2_Click(object sender, EventArgs e)
         {
-        /*   // if (textBox2.Text != null)
-           // {
+            if (dataGridView1 != null)
+            {
                 if (saveFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-
-                    var path = saveFileDialog1.FileName;
-
+                  
                     XDocument xdoc = new XDocument();
                     XElement source = new XElement("source");                    
-                    XAttribute addressAttr = new XAttribute("address", serial);
+                    XAttribute addressAttr = new XAttribute("address", dataGridView1.Rows[1].Cells[4].Value);//fix it!
                     XAttribute speedAttr = new XAttribute("speed", "unknown");
 
                     XElement line = new XElement("line");
                     XAttribute directionAttr = new XAttribute("direction", "request");
-                    XAttribute addressLineAttr = new XAttribute("address", address4);
+                    XAttribute addressLineAttr = new XAttribute("address", dataGridView1.Rows[1].Cells[4]);
                     XAttribute commandAttr = new XAttribute("command", "46:Read EEPROM");
                     XAttribute crcAttr = new XAttribute("crc","CA6C");
 
                     XElement raw_frame = new XElement("raw_frame");
-                    raw_frame.Value = address;//fix it 
+                    raw_frame.Value = "";//fix it 
 
                     // добавляем атрибут и элементы в первый элемент
                     source.Add(addressAttr);
@@ -229,8 +232,8 @@ namespace LogMODBUS
                     //сохраняем документ
                     xdoc.Save("Log.xml");
                 }
-           // }
-           */
+            }
+           
         }
        
 
